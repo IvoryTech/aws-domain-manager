114c70d433f775717f6f203d0622ceb1902f45ad
chengjiaapraamcos <86397722+chengjiaapraamcos@users.noreply.github.com>
2022-01-05 16:57:26 +1100

    try

--- index.ts
+++ index.ts
@@ -1,1 +1,26 @@
-console.log("hello");
+import { createDomain } from "src/createDomain";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+interface IArgs {
+    domainName: string,
+    dnsName: "CloudFront" | string,
+    domainAccountId?: string,
+    roleName?: string
+}
+
+
+const argv = (yargs(hideBin(process.argv)).argv) as unknown as IArgs
+console.log(argv);
+
+if (!argv.dnsName) {
+    throw new Error("Missing dnsName");
+}
+
+if (!argv.domainName) {
+    throw new Error("Missing domainName");
+}
+
+(async() => {
+   await createDomain(argv);
+})()
---
+++ src/createDomain.ts
@@ -0,0 +1,52 @@
+import {
+  Route53Client,
+  ChangeResourceRecordSetsCommand,
+  ListHostedZonesCommand
+} from "@aws-sdk/client-route-53";
+import { getAssumeRoleCredentials } from "./helper";
+
+export const createDomain = async (
+  {domainName, dnsName, domainAccountId, roleName}:
+  {domainName: string, dnsName: "CloudFront" | string, domainAccountId?: string,roleName?: string }
+) => {
+  const route53Client = new Route53Client({
+    credentials: domainAccountId && roleName ? await getAssumeRoleCredentials(domainAccountId, roleName) : undefined });
+
+  const hostedZoneId = (await route53Client.send(new ListHostedZonesCommand({}))).HostedZones?.find(
+    (x) => x.Name?.includes(domainName.split(".").slice(1).join("."))
+  )?.Id;
+
+  const response = await route53Client.send(
+    new ChangeResourceRecordSetsCommand({
+      HostedZoneId: hostedZoneId,
+      ChangeBatch: {
+        Changes: [
+          {
+            Action: "UPSERT",
+            ResourceRecordSet: {
+              Name: domainName,
+              Type: "A",
+              AliasTarget: {
+                DNSName: dnsName,
+                HostedZoneId: "Z2FDTNDATAQYW2",
+                EvaluateTargetHealth: false
+              }
+            }
+          },
+          {
+            Action: "UPSERT",
+            ResourceRecordSet: {
+              Name: domainName,
+              Type: "AAAA",
+              AliasTarget: {
+                DNSName: dnsName,
+                HostedZoneId: "Z2FDTNDATAQYW2",
+                EvaluateTargetHealth: false
+              }
+            }
+          }
+        ]
+      }
+    })
+  );
+};
